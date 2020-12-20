b5e0bb9a43a67186d6d00fadee3bc94d17521a61
pythagoras-dev <mubarakmustapha15@gmail.com>
2020-12-20 19:35:49 +0100

    added navbar component

---
+++ Navbar Component/src/Navbar.js
@@ -0,0 +1,59 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  // for the links div
+  const linksContainerRef = useRef(null)
+  // for the links
+  const linksRef = useRef(null)
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <h3>Pythagoras-Dev</h3>
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((social_icon) => {
+            const { id, url, icon } = social_icon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
